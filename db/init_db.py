a7303cdeafe9c23d837d610dc1cb46b49ffa2b66
jonca3d <jonca3d@gmail.com>
2020-10-23 22:01:52 +0300

    Исправленны некоторые запросы на создание таблиц, в которых был указан неверынй тип данных

--- db/init_db.py
+++ db/init_db.py
@@ -15,54 +15,66 @@ def init_db_tables():
     Назначение функции проинициализировать БД при первом старте бота.
     :return:
     """
-    cur = conn.cursor()
+    cursor = conn.cursor()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS users('
-                'telegram_id INTEGER PRIMARY KEY, '
-                'first_name VARCHAR(40), '
-                'last_name VARCHAR(40), '
-                'timezone INTEGER'
-                ');')
+    cursor.execute('CREATE TABLE IF NOT EXISTS users('
+                   'telegram_id INTEGER PRIMARY KEY, '
+                   'first_name VARCHAR(40), '
+                   'last_name VARCHAR(40), '
+                   'timezone INTEGER'
+                   ');')
     conn.commit()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS subscriptions('
-                'id SERIAL PRIMARY KEY, '
-                'name VARCHAR(40), '
-                'description TEXT'
-                ');')
+    cursor.execute('CREATE TABLE IF NOT EXISTS subscriptions('
+                   'id INTEGER PRIMARY KEY, '
+                   'name VARCHAR(40), '
+                   'description TEXT'
+                   ');')
     conn.commit()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS user_subs('
-                'user_id INTEGER NOT NULL, '
-                'subscription_id INTEGER NOT NULL, '
-                'status INTEGER, '
-                'FOREIGN KEY("user_id") REFERENCES "users"("telegram_id"),'
-                'FOREIGN KEY("subscription_id") REFERENCES "subscriptions"("id")'
-                ');')
+    cursor.execute('CREATE TABLE IF NOT EXISTS user_subs('
+                   'user_id INTEGER NOT NULL, '
+                   'subscription_id INTEGER NOT NULL, '
+                   'status INTEGER, '
+                   'FOREIGN KEY("user_id") REFERENCES "users"("telegram_id"),'
+                   'FOREIGN KEY("subscription_id") REFERENCES "subscriptions"("id")'
+                   ');')
     conn.commit()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS options_daily('
-                'user_id INTEGER PRIMARY KEY, '
-                'start_time VARCHAR(20),'
-                'finish_time VARCHAR(20), '
-                'how_many_time INTEGER NOT NULL'
-                ');')
+    cursor.execute('CREATE TABLE IF NOT EXISTS options_daily('
+                   'user_id INTEGER PRIMARY KEY, '
+                   'start_time VARCHAR(20),'
+                   'finish_time VARCHAR(20), '
+                   'how_many_time INTEGER NOT NULL'
+                   ');')
     conn.commit()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS mailer_list('
-                'id SERIAL PRIMARY KEY, '
-                'time_id INTEGER NOT NULL, '
-                'user_id INTEGER NOT NULL, '
-                'subscription_id INTEGER NOT NULL'
-                ');')
+    cursor.execute('CREATE TABLE IF NOT EXISTS mailer_list('
+                   'id SERIAL PRIMARY KEY, '
+                   'time_id VARCHAR(10) NOT NULL, '
+                   'user_id INTEGER NOT NULL, '
+                   'subscription_id INTEGER NOT NULL'
+                   ');')
     conn.commit()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS daily_tasks('
-                'id SERIAL PRIMARY KEY, '
-                'user_id INTEGER NOT NULL, '
-                'description VARCHAR(200) NOT NULL, '
-                'time_stamp INTEGER NOT NULL, '
-                'status INTEGER NOT NULL, '
-                'FOREIGN KEY("user_id") REFERENCES "users"("telegram_id")'
-                ');')
+    cursor.execute('CREATE TABLE IF NOT EXISTS daily_tasks('
+                   'id SERIAL PRIMARY KEY, '
+                   'user_id INTEGER NOT NULL, '
+                   'description VARCHAR(200) NOT NULL, '
+                   'time_stamp INTEGER NOT NULL, '
+                   'status INTEGER NOT NULL, '
+                   'FOREIGN KEY("user_id") REFERENCES "users"("telegram_id")'
+                   ');')
     conn.commit()
+
+
+def init_db_data():
+    cursor = conn.cursor()
+    cursor.execute('SELECT EXISTS(SELECT * FROM subscriptions WHERE id = 1)')
+    result = cursor.fetchone()
+    if not result[0]:
+        cursor.execute('INSERT INTO subscriptions(id, name, description) '
+                       'VALUES(%s, %s, %s);',
+                       (1, "daily", "Каждый день отправляет запрос пользователю на список дел заданное "
+                                    "колличесво раз за день напоминает о незавершенных делах"))
+        conn.commit()
